a841d90632b756dd0c9b5cabd9d78219e448a6bc
Kuutniemi <jkuutniemi@outlook.com>
2022-04-04 21:25:35 +0300

    part 3 alotettu

---
+++ contacts.js
@@ -0,0 +1,24 @@
+export const yhteystiedot = {
+  persons: [
+    {
+      name: "12312312",
+      number: "123123123",
+      id: 5,
+    },
+    {
+      name: "Joku",
+      number: "JSAKdja",
+      id: 10,
+    },
+    {
+      name: "Niujja noiui",
+      number: "1234",
+      id: 15,
+    },
+    {
+      name: "Ei logeja",
+      number: "Joasd",
+      id: 16,
+    },
+  ],
+};
