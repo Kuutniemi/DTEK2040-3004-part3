1bb1b4185382402add5c086f6c5c64229def6c1d
Kuutniemi <jkuutniemi@outlook.com>
2022-04-10 16:53:35 +0300

    added frontend build

---
+++ build/precache-manifest.9966f38f6f86055e1093a2304b4304f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fccdbf572c8503cc563a",
+    "url": "/static/css/main.25dcb48c.chunk.css"
+  },
+  {
+    "revision": "fccdbf572c8503cc563a",
+    "url": "/static/js/main.fccdbf57.chunk.js"
+  },
+  {
+    "revision": "d99a26fe0d097c75064b",
+    "url": "/static/js/1.d99a26fe.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "278cef85ebaef40bdd81040b9b388055",
+    "url": "/index.html"
+  }
+];
--- mongo.js
+++ mongo.js
@@ -6,37 +6,14 @@ const url = process.env.MONGO;
 
 mongoose.connect(url);
 
-let data = [];
-
 const Yhteystieto = mongoose.model("Yhteystieto", {
   name: String,
   number: Number,
-  id: Number,
 });
-
-const update = Yhteystieto.find({}).then((result) => {
-  result.forEach((yht) => {
-    data.push(yht);
-  });
-  mongoose.connection.close();
-  //console.log(data);
-});
-
-const generateId = () => {
-  const maxId =
-    data.length > 0
-      ? data
-          .map((n) => n.id)
-          .sort((a, b) => a - b)
-          .reverse()[0]
-      : 1;
-  return maxId + 1;
-};
 
 const yhteystieto = new Yhteystieto({
   name: process.argv[2],
   number: process.argv[3],
-  id: generateId(),
 });
 
 if (process.argv[2] !== undefined || process.argv[3] !== undefined) {
@@ -48,13 +25,13 @@ if (process.argv[2] !== undefined || process.argv[3] !== undefined) {
       process.argv[3],
       "lisattiin onnistuneesti"
     );
-    update, console.log(data), mongoose.connection.close();
+    mongoose.connection.close();
   });
 } else {
   console.log("Yhteystiedot:");
   Yhteystieto.find({}).then((result) => {
     result.forEach((yht) => {
-      console.log("Nimi", yht.name, "Numero:", yht.number, yht.id);
+      console.log("Nimi", yht.name, "Numero:", yht.number);
     });
     mongoose.connection.close();
   });
